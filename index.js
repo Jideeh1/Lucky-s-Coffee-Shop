c491b2fd17275da9fc5f903fe98b53ac1319092f
John Daniel Absalud <keonagjd@gmail.com>
2025-09-14 15:50:59 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('Happy developing ✨')
+
+ const choices = document.querySelectorAll(".choices a");
+  const items = document.querySelectorAll(".item-box");
+
+  choices.forEach(choice => {
+    choice.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      choices.forEach(c => c.style.color = "#b7a78d");
+
+      this.style.color = "#6B4F1C";
+
+      const category = this.dataset.category;
+      items.forEach(item => {
+        if (item.classList.contains(category)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  });
